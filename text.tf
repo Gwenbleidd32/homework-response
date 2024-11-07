cfa4e915760c490da8231f26c3c754ffc52e4c2f
gwenbleidd@32 <premiumforges32@gmail.com>
2024-11-07 00:28:14 -0800

    visual reference

---
+++ text.tf
@@ -0,0 +1,49 @@
+/*
+              San-Francisco                             Tokyo
+                     \          User Traffic            / 
+                      \-------(Global Requests)--------/
+                                      |       ↑       
+                                      |       ↑       
+                                      |       ↑
+---VPC--------------------------------▼-------↑--------------------------------                            
+    buckets not included enabled with private access and serverless access                      
+                                      │       ↑
+                                      │       ↑
+                                      │       ↑
+                                      │       ↑ 
+                                      │----------                           
+                                      │ Cloud CDN -> Cached content routed through edgepoints
+                                      │----------
+                       ┌─────────────────────────────┐ 
+                       │    HTTP(S) Load Balancer    │
+                       │   (Routes Requests to the   │
+                       │     Appropriate Backend)    │ 
+                       └──────────────┬──────────────┘ 
+                                      │                 
+              ┌───────────────────────▼──────────────────────┐
+              │                                              │
+              │                                              │
+┌─────────────▼────────────┐                   ┌─────────────▼────────────┐
+│        Bucket 1          │                   │         Bucket 2         │-> Multi Region Buckets
+│      (US Region)         │                   │       (Asia Region)      │
+│    Stores Static HTML    │                   │    Stores Static HTML    │
+└────────────────────────↑─┘                   └──↑───────────────────────┘
+                          \                      /           
+                           \                    /-> Cloud run deploys WordPress Generated HTML to static Host             
+------US-WEST2-BASED SUBNET-\------------------/-------------------------------                          
+                             \                /                
+                  ┌───────────\──────────────/───────────┐
+                  │              Cloud Run               │
+                  │     (WordPress image with static     │- Instance serves as lightweight containeriz host
+                  │           Content Exports)           │
+                  └──────────────────────────────────────┘
+                                      │                       
+                                      │ -> Wordpress image uses managed Cloud SQL DB for infrequent writes.                      
+                                      │    
+                  ┌───────────────────▼───────────────────┐
+                  │             Cloud SQL                 │
+                  │   (MySQL Database with Private IP)    │
+                  │       Secure Access in VPC)           │
+                  └───────────────────────────────────────┘
+-------------------------------------------------------------------------------
+*/
